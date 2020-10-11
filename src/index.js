9cb421f64ab40cb4368294802da5572de2840a77
Alex <qatechnix@gmail.com>
2020-10-11 18:29:53 +0700

    feat: add brackets task colution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsArray = [];
+  for(let i = 0; i < bracketsConfig.length; i++){
+    bracketsArray[i] = bracketsConfig[i].join('')
+  }
+  for(let j = 0; j < bracketsArray.length; ){
+    if(str.indexOf(bracketsArray[j]) >= 0) {
+      str = str.replace(bracketsArray[j], '')
+      j = 0;
+    }
+    else {
+      j++;
+    }
+  }
+  if(str.length === 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
